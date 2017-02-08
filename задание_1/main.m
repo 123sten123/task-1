37b5508863fb3bad25dc372c0d3a075436960ebd
123sten123 <pozolotin.stepan@yandex.ru>
2017-02-08 12:08:47 +0400

    задание1

---
+++ задание_1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  задание_1
+//
+//  Created by стен on 31.01.17.
+//  Copyright (c) 2017 стен. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
